37867b10dd9f05322a64a1bff02f037155fe033f
Xurxo Rigueira Díaz <xrigueiradiaz@gmail.com>
2023-11-01 19:35:55 -0700

    Added resources

--- test.py
+++ test.py
@@ -1,26 +1,13 @@
-import warnings
-warnings.filterwarnings('ignore')
+# Paper in which a trasformer was used for time series prediction https://arxiv.org/abs/2001.08317
+# Adapted code from that paper https://github.com/KasperGroesLudvigsen/influenza_transformer/tree/main
+# Seohye code https://github.com/GW-ASU/LR-Multi-HeadAttention/blob/main/Transformer_timeadd_log_layers.ipynb
+# Tutorial https://huggingface.co/blog/time-series-transformers
+# Questions that may help me with encoding https://discuss.pytorch.org/t/using-transformer-on-timeseries/104759/6
 
-from data_provider.data_loader import data_provider
-
-# Define arguments
-args = {
-        'data': 'weather',
-        'root_path': './data/weather/',
-        'data_path': 'weather.csv',
-        'features': 'M',
-        'target': 'OT',
-        'freq': '10min',
-        'checkpoints': './checkpoints/',
-        'seq_len': 96,
-        'label_len': 48,
-        'pred_len': 96,
-        'embed': 'timeF',
-        'num_workers': 8,
-        'batch_size': 32
-    }
-
-if __name__ == '__main__':
-
-    data_set, data_loader = data_provider(args, flag='train')
-    print(data_set)
+# I have to watch PyTorch tutorials and then go back to the influenza code and lastly Seohye
+# https://pytorch.org/tutorials/beginner/basics/intro.html
+# https://pytorch.org/tutorials/beginner/deep_learning_60min_blitz.html
+# https://pytorch.org/tutorials/beginner/introyt.html
+# https://pytorch.org/tutorials/beginner/pytorch_with_examples.html
+# https://pytorch.org/tutorials/beginner/nn_tutorial.html
+# https://www.youtube.com/watch?v=c36lUUr864M&ab_channel=PatrickLoeber
