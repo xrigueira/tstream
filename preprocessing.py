e2b31bac2c535ffe7b6eda3740adde91e6a69280
Xurxo Rigueira <xrigueiradiaz@gmail.com>
2024-03-07 11:37:30 +0100

    Fix superfluous file save

--- preprocessing.py
+++ preprocessing.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 # variables = 'masked_SWIT'
 variables = 'unmasked_SWIT_PET'
 
-if variables == 'masked_PET':
+if variables == 'masked_SWIT':
 
     # Load raw X data (snow melted + precipitation) - just element X1 of the loaded matlab object
     raw_data = scipy.io.loadmat('data/SWIT_28x18.mat')['X1']
@@ -46,7 +46,7 @@ else:
 
     # Load raw X data (snow melted + precipitation) - just element 'data' of the loaded matlab object
     raw_data = scipy.io.loadmat('data/SWIT_PET_y_masked.mat')['data']
-
+    
     # Build SWIT, PET and Q data from data_for_ATT_time
     # Get the first window of SWIT and PET and Q
     data = raw_data[0][:,(0,1,-1)]
@@ -55,21 +55,14 @@ else:
     for i in range(1, len(raw_data)):
         data = np.append(data, [raw_data[i][-1,(0,1,-1)]], axis=0) # Reshape the second array to have the same number of dimensions
 
-    # Save the data to a csv file
-    np.savetxt('data/SWIT_PET_y_masked.csv', data, delimiter=',', fmt='%f')
-
-    # Load the data
-    raw_data = pd.read_csv('data/SWIT_PET_y_masked.csv')
-
-    # Define the column names
-    raw_data.columns = ['x1', 'x2', 'y']
-
+    data = pd.DataFrame(data, columns=['x1', 'x2', 'y'])
+    
     # Generate dates starting from Jan 2nd, 1980
     start_date = '1980-01-01'
-    raw_data['time'] = pd.date_range(start=start_date, periods=len(raw_data), freq='D')
+    data['time'] = pd.date_range(start=start_date, periods=len(data), freq='D')
 
     # Change column names
-    raw_data = raw_data[['time', 'x1', 'x2', 'y']]  # Reorder columns
+    data = data[['time', 'x1', 'x2', 'y']]  # Reorder columns
 
     # Save the data
-    raw_data.to_csv('data/data_2.csv', sep=',', index=False, encoding='utf-8')
+    data.to_csv('data/data.csv', sep=',', index=False, encoding='utf-8')
