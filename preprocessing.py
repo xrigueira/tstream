f073163327156e1112d511e3cca8eaf9b481e32a
Xurxo Rigueira <xrigueiradiaz@gmail.com>
2024-03-07 11:05:08 +0100

    Update preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -3,13 +3,13 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-variables = 'PET'
-variables = 'PETSWIT'
+# variables = 'masked_SWIT'
+variables = 'unmasked_SWIT_PET'
 
-if variables == 'PET':
+if variables == 'masked_PET':
 
     # Load raw X data (snow melted + precipitation) - just element X1 of the loaded matlab object
-    raw_data = scipy.io.loadmat('data/X_28x18.mat')['X1']
+    raw_data = scipy.io.loadmat('data/SWIT_28x18.mat')['X1']
 
     # Load mask - element ismap of the loaded matlab object
     mask = scipy.io.loadmat('data/mask_28x18.mat')['ismap']
@@ -18,7 +18,7 @@ if variables == 'PET':
     X = np.average(raw_data * mask, axis=(2, 3))
 
     # Save the masked data
-    np.save('data/X_masked.npy', X, allow_pickle=False, fix_imports=False)
+    np.save('data/SWIT_masked.npy', X, allow_pickle=False, fix_imports=False)
 
     # Load y data (stream flow) - element 'Q1' of the loaded matlab object
     y = scipy.io.loadmat('data/y.mat')['Q1']
@@ -43,14 +43,29 @@ if variables == 'PET':
     df.to_csv('data/data.csv', sep=',', index=False, encoding='utf-8')
 
 else:
+
+    # Load raw X data (snow melted + precipitation) - just element 'data' of the loaded matlab object
+    raw_data = scipy.io.loadmat('data/SWIT_PET_y_masked.mat')['data']
+
+    # Build SWIT, PET and Q data from data_for_ATT_time
+    # Get the first window of SWIT and PET and Q
+    data = raw_data[0][:,(0,1,-1)]
+
+    # Loop through the rest of the windows and append the last row
+    for i in range(1, len(raw_data)):
+        data = np.append(data, [raw_data[i][-1,(0,1,-1)]], axis=0) # Reshape the second array to have the same number of dimensions
+
+    # Save the data to a csv file
+    np.savetxt('data/SWIT_PET_y_masked.csv', data, delimiter=',', fmt='%f')
+
     # Load the data
-    raw_data = pd.read_csv('data/PET_SWIT.csv')
+    raw_data = pd.read_csv('data/SWIT_PET_y_masked.csv')
 
     # Define the column names
     raw_data.columns = ['x1', 'x2', 'y']
 
     # Generate dates starting from Jan 2nd, 1980
-    start_date = '1980-01-02'
+    start_date = '1980-01-01'
     raw_data['time'] = pd.date_range(start=start_date, periods=len(raw_data), freq='D')
 
     # Change column names
--- test.py
+++ test.py
@@ -3,13 +3,16 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Load the data
-# Load raw X data (snow melted + precipitation) - just element X1 of the loaded matlab object
-# raw_data = scipy.io.loadmat('data/data_for_ATT_time.mat')['data']
-# print(raw_data.shape)
+# Load raw X data (snow melted + precipitation) - just element 'data' of the loaded matlab object
+raw_data = scipy.io.loadmat('data/SWIT_PET_y_masked_v3.mat')['data']
 
-# raw_data = scipy.io.loadmat('data/data_for_ATT_time1y_v3.mat')['data']
-# print(raw_data.shape)
+# Build SWIT, PET and Q data from data_for_ATT_time
+# Get the first window of SWIT and PET and Q
+data = raw_data[0][:,(0,1,-1)]
 
-raw_data = scipy.io.loadmat('data/X_28x18.mat')['X1']
-print(raw_data)
+# Loop through the rest of the windows and append the last row
+for i in range(1, len(raw_data)):
+    data = np.append(data, [raw_data[i][-1,(0,1,-1)]], axis=0) # Reshape the second array to have the same number of dimensions
+
+# Save the data to a csv file
+np.savetxt('data/SWIT_PET_y_masked.csv', data, delimiter=',', fmt='%f')
